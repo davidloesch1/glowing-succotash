94d3f4022d609929c91cb4f59d3ecc5afa283aba
David <davidloesch1@gmail.com>
2022-11-12 11:08:45 -0800

    adding safe fixing_safe_area

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, ThemeProvider } from 'react-native-elements';
-
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, ThemeProvider } from "react-native-elements";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <Button title="Hey!" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <Button title="Hey!" />
+        <Text>tests</Text>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
